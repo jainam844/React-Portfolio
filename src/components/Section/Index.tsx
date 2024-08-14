d8f019c2a3f4bbd28e1a7f247c0b1fb67e1609bb
JAINAM SHAH <84221238+jainam844@users.noreply.github.com>
2024-08-14 09:29:54 +0530

    Update Index.tsx

--- src/components/Section/Index.tsx
+++ src/components/Section/Index.tsx
@@ -84,20 +84,23 @@ const Index: React.FC<IndexProps> = ({ id }) => {
                     data-aos="fade-left"
                     data-aos-delay="600"
                 >
-                    <Typography variant="h4" component="h1" sx={{ mb: 2 }} color={"#1976d2"} fontWeight={600}>
+               <Typography variant="h4" component="h1" sx={{ mb: 2 }} color={"#1976d2"} fontWeight={600}>
                         ⚡ Full Stack <span style={{ color: 'black' }}>Web Developer!</span>
                     </Typography>
                     <Typography component="p" sx={{ mb: 2, fontWeight: 500 }}>
-                        Welcome to my portfolio! I'm a passionate Full Stack Web Developer from India, currently pursuing a Bachelor of Engineering in Computer Engineering. With expertise in React, Node.js, Express.js, MongoDB, HTML, CSS, Git, and GitHub, I specialize in creating dynamic, user-friendly web applications.
+                        Welcome to my portfolio! I’m a passionate Full Stack Web Developer from India, holding a Bachelor of Engineering in Computer Engineering. I have hands-on experience developing full-stack applications using React, Angular, Node.js, Express.js, and MongoDB, with a focus on the MERN stack.
                     </Typography>
                     <Typography variant="body1" component="p" sx={{ mb: 2, fontWeight: 500 }}>
-                        My journey in technology has been fueled by an insatiable curiosity and a commitment to continuous learning. Whether it's designing sleek user interfaces or developing efficient backend systems, I am dedicated to delivering high-quality, innovative solutions.
+                        My journey in technology has been driven by an insatiable curiosity and a commitment to continuous learning. I thrive on exploring new technologies and incorporating them into my projects, always aiming to create dynamic, user-friendly applications.
                     </Typography>
                     <Typography variant="body1" component="p" sx={{ mb: 2, fontWeight: 500 }}>
-                        Whenever possible, I also apply my passion for developing products with Node.js and modern JavaScript libraries and frameworks like React.js and Next.js.
+                        As a self-motivated and hardworking individual, I am dedicated to delivering high-quality, innovative solutions, whether it’s designing sleek user interfaces or developing robust backend systems.
                     </Typography>
                     <Typography variant="body1" component="p" sx={{ mb: 2, fontWeight: 500 }}>
-                        Stay tuned as I share my latest projects and insights in the world of web development.
+                        Whenever possible, I apply my passion for developing products with modern JavaScript libraries and frameworks like React and Angular, alongside my knowledge of Node.js and Express.js.
+                    </Typography>
+                    <Typography variant="body1" component="p" sx={{ mb: 2, fontWeight: 500 }}>
+                        Stay tuned as I share my latest projects and insights in the ever-evolving world of web development.
                     </Typography>
                 </Box>
             </Box>
